5f4734d462b3981a17534c40ce2af40723fe93b3
John Hughes <hughesj@umn.edu>
2015-01-16 00:00:00 +0000

    version 1.0-1

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 #
 # Written by John Hughes <hughesj@umn.edu>.
 #
-# Last Modified 02/11/13
+# Last Modified 01/16/15
 # Licensed under the GNU General Public License version 2 (June, 1991)
 #
 # Part of the R/pearson7 package
@@ -20,8 +20,7 @@
     temp = packageDescription("pearson7")
     msg = paste(temp$Package, ": ", temp$Title, "\n", "Version ", temp$Version,
                 " created on ", temp$Date, ".\n", sep = "")
-    msg = paste(msg, "copyright (c) 2013, John Hughes, University of Minnesota\n",
-#"                    Xiaohui Cui, University of Minnesota\n",
+    msg = paste(msg, "copyright (c) 2013-15, John Hughes, University of Minnesota\n",
                 sep = "")
     msg = paste(msg, 'For citation information, type citation("pearson7").\n', sep = "")
     msg = paste(msg, 'Type help(package = pearson7) to get started.\n', sep = "")
