f79ad4be54c308d6b00ccd80be27c25f6ec6508d
John Hughes <jphughesjr@gmail.com>
2016-06-22 05:42:12 +0000

    version 1.0-2

--- R/zzz.R
+++ R/zzz.R
@@ -2,25 +2,27 @@
 #
 # zzz.R
 #
-# Written by John Hughes <hughesj@umn.edu>.
+# Written by John Hughes <jphughesjr@gmail.com>.
 #
-# Last Modified 01/16/15
+# Last Modified 06/21/16
 # Licensed under the GNU General Public License version 2 (June, 1991)
 #
 # Part of the R/pearson7 package
 #
-# .onLoad is run when the package is loaded with library(pearson7)
-#
 ######################################################################
 
 #' @import utils
+#' @importFrom stats median
+#' @importFrom stats qnorm
+#' @importFrom stats rexp
+#' @importFrom stats rnorm
 
 .onAttach = function(libname, pkgname)
 {
     temp = packageDescription("pearson7")
     msg = paste(temp$Package, ": ", temp$Title, "\n", "Version ", temp$Version,
                 " created on ", temp$Date, ".\n", sep = "")
-    msg = paste(msg, "copyright (c) 2013-15, John Hughes, University of Minnesota\n",
+    msg = paste(msg, "copyright (c) 2013-2016, John Hughes, University of Colorado Denver\n",
                 sep = "")
     msg = paste(msg, 'For citation information, type citation("pearson7").\n', sep = "")
     msg = paste(msg, 'Type help(package = pearson7) to get started.\n', sep = "")
